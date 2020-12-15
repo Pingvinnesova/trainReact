2bff1b0d91f8bb9b743663e07150709272777579
Polosat <ekaterina.mihnevich@mail.ru>
2020-12-15 07:24:19 +0300

    Подготавливает проект

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = () => {
+    return <h2>Hello, World</h2>
+}
+
+const Field = () => {
+    const holder = 'Enter here';
+    return <input type="text" placeholder={holder} />
+}
+
+const Button = () => {
+    const text = 'Log-in';
+    return <button>{text}</button>
+    // const res = () => {
+    //     return 'Log-in, please'
+    // }
+    // const p = <p>Log-in, quick!</p>
+    // return <button>{p}</button>
+    // return <button>{res()}</button>
+
+    // return <button>{3+4}</button>
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Field/>
+            <Button/>
+        </div>
+    )
+}
+
+
+
+
+// const elem = React.createElement('h2', null, "Hello, World!!!");
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
